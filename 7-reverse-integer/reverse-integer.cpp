8335425cf8dbc2168beebf279d81c75026b6ec51
Mannat <mannat.garglic@gmail.com>
2026-01-12 18:25:25 +0530

    Time: 5 ms (20.18%) | Memory: 8.4 MB (97.10%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long sum = 0;
+        while(x != 0){
+            int digit = x%10;
+            x /= 10;
+
+            if(sum > INT_MAX/10 || (sum == INT_MAX/10 && digit > 7))
+            return 0;
+            if(sum < INT_MIN/10 || (sum == INT_MIN/10 && digit < -8))
+            return 0;
+
+            sum = sum*10 + digit;
+        }
+        return sum;
+    }
+};
