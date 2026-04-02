ea860c30890f2ab7791ea355067f9b99e24fd7b3
Mannat <mannat.garglic@gmail.com>
2026-04-02 23:40:28 +0530

    Time: 14 ms (49.70%) | Memory: 41.8 MB (95.23%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -5,19 +5,17 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int beg = 1;
         int end = n;
-        int mid;
-        int ans = -1;
-        while(beg<=end){
-            mid = beg + (end - beg) / 2;;
+        
+        while(beg<end){
+           int mid = beg + (end - beg) / 2;;
             if(isBadVersion(mid)){
-                end = mid - 1 ;
-                ans = mid;
+                end = mid ;
             }
             else {
                   beg = mid+1;
             }
 
         }
-        return ans;
+        return beg;
     }
 }
