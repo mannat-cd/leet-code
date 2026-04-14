c869ab2fb10484e0db5ce8c5217edb6ee2257c6d
Mannat <mannat.garglic@gmail.com>
2026-04-14 20:43:23 +0530

    Time: 3 ms (61.43%) | Memory: 48.5 MB (56.37%) - LeetSync

---
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int n = matrix.length;
+        int ans[] = new int[n];
+        for(int i = 0; i<n; i++){
+            int a =0;
+            for(int j = 0; j<n; j++){
+               if(matrix[i][j] == 1){
+                a++;
+               }
+            
+            }
+            ans[i] = a;
+        }
+        return ans;
+    }
+}
