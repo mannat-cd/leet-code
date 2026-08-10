ace4a93c7e6886b149ca4583776eb6124353514b
Mannat <mannat.garglic@gmail.com>
2026-08-10 22:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (94.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=i+1; j<matrix[0].size(); j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+         int n = matrix.size();
+            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+        
+    }
+};
